bca9925d9e7a7982553ad1da54e27ab7151fb7d2
Haim Shragai <haimke11@gmail.com>
2021-11-10 13:06:58 +0200

    haim update

--- scraper.py
+++ scraper.py
@@ -24,7 +24,6 @@ CHANNEL_CHOICE = CFG.CHANNEL_CHOICE
 PAGES = CFG.PAGES
 
 
-
 def get_next_page(current_page):
     """
     returns the soup of the next page
@@ -47,13 +46,13 @@ def get_next_page(current_page):
         return BeautifulSoup(next_page.text, 'html.parser')
 
 
-def get_data(post_input):
+def get_post_data(post_input):
     """
-    Returns the following data from an individual post:
+    Returns the following data from an individual post soup object:
     title, number of comments, number of likes, the date of the post
     Parameters
     ----------
-    post_input : the post to get the data from
+    post_input : the post soup object to get the data from
 
     Returns
     -------
@@ -70,8 +69,41 @@ def get_data(post_input):
     return title, comments, likes, post_date
 
 
+def get_user_data(user_url):
+    user_data = []
+
+    for sort in ['new', 'top']:
+        comment_data = []
+        post_data = []
+
+        page = requests.get(f'{user_url}/comments/?sort={sort}', headers=HEADERS)
+        soup = BeautifulSoup(page.content, 'html.parser')
+
+        for comment in soup.select('div[class*="thing"]'):
+            title = comment.find(class_='title').text
+            author = comment.select('a[class*="author"]')[0].text
+            thread = comment.find(class_='subreddit hover').text
+            # points = comment.find(class_='score unvoted').text
+            date = comment.find(class_='live-timestamp').attrs['datetime'].split('T')[0]
+            text = comment.find(class_='md').find('p').text
+            comment_data.append((title, author, thread, date, text))
+
+        page = requests.get(f'{user_url}/submitted/?sort={sort}', headers=HEADERS)
+        soup = BeautifulSoup(page.content, 'html.parser')
+
+        for post in soup.select('div[class*="thing"]'):
+            thread = post.find(class_='subreddit hover may-blank').text
+            post_data.append(tuple(list(get_post_data(post)) + [thread]))
+
+        user_data.append((f'{sort} comments', comment_data))
+        user_data.append((f'{sort} posts', post_data))
+
+    return user_data
+
+
 def main():
     data = list()
+    users = dict()
 
     for index, channel in enumerate(CHANNELS):
         data.append([])
@@ -86,7 +118,11 @@ def main():
                 not_promotion = post.find('span', class_="promoted-span") is None
                 not_announcement = post.find('span', class_="stickied-tagline") is None
                 if not_promotion and not_announcement:
-                    data[index].append(get_data(post))
+                    data[index].append(get_post_data(post))
+
+                    username = post.select('a[class*="author"]')[0].text
+                    if username not in users.keys():
+                        users[username] = get_user_data(post.select('a[class*="author"]')[0].attrs['href'])
 
             soup = get_next_page(soup)
             counter += 1
